e0dd1542870fbd12e7678cb2285fc3f86f4dfc9b
ZoeyYingying <Zoey13652755443@gmail.com>
2021-10-01 22:33:00 -0600

    Finished week3_TV

--- app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
@@ -3,6 +3,11 @@ package algonquin.cst2335.zhao0211;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgView;
+        Switch sw;
+
+            imgView = findViewById(R.id.imageView2);
+            sw = findViewById(R.id.switch1);
+
+            sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+                if (isChecked)
+                {
+                    RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setDuration(5000);
+                    rotate.setRepeatCount(Animation.INFINITE);
+                    rotate.setInterpolator(new LinearInterpolator());
+
+                    imgView.startAnimation(rotate);
+                }
+                else {
+                    imgView.clearAnimation();
+                }
+
+
+            });
     }
 }
