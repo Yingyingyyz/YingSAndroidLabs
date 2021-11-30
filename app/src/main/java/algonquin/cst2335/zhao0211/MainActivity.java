553ed7c0aeed59fe223fac3f932a2be5e1a7c23c
ZoeyYingying <Zoey13652755443@gmail.com>
2021-11-29 19:10:35 -0700

    Finished Week9 in JSON

--- app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
@@ -1,76 +1,136 @@
 package algonquin.cst2335.zhao0211;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.BitmapCompat;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String stringUrl = "https://api.openweathermap.org/data/2.5/weather?q=Los+Angeles&appid=7e943c97096a9784391a981c4d878b22&Unit=Metric";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText emailEdt = findViewById(R.id.email_edit);
 
-        // get local data
-        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-        String email = prefs.getString("email", "");
-        // set email
-        emailEdt.setText(email);
+        EditText cityTextField = findViewById(R.id.cityTextField);
+        Button btn = findViewById(R.id.forecastButton);
 
-        // login button click
-        findViewById(R.id.login_btn).setOnClickListener(v -> {
-            // save to local
-            String emailStr = emailEdt.getText().toString();
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("email", emailStr);
-            editor.apply();
+        btn.setOnClickListener((View v) -> {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(() -> {
+                try {
+                    // Los Angeles
+                    String cityName = cityTextField.getText().toString();
+                    stringUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+                            + URLEncoder.encode(cityName, "UTF-8")
+                            + "&appid=7e943c97096a9784391a981c4d878b22&unit=metric";
 
+                    URL url = new URL(stringUrl);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = httpURLConnection.getInputStream();
 
-            // to SecondActivity
-            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-            nextPage.putExtra("email", emailStr);
-            startActivity(nextPage);
+                    String text = (new BufferedReader(
+                            new InputStreamReader(in, StandardCharsets.UTF_8)))
+                            .lines()
+                            .collect(Collectors.joining("\n"));
+
+                    JSONObject theDocument = new JSONObject(text);
+
+                    JSONArray weatherArray = theDocument.getJSONArray("weather");
+                    JSONObject position0 = weatherArray.getJSONObject(0);
+                    String iconName = position0.getString("icon");
+                    String description = position0.getString("description");
+
+                    JSONObject mainObject = theDocument.getJSONObject("main");
+                    double current = mainObject.getDouble("temp");
+                    double min = mainObject.getDouble("temp_min");
+                    double max = mainObject.getDouble("temp_max");
+                    int humidity = mainObject.getInt("humidity");
+
+                    Bitmap image = null;
+                    File file = new File(getFilesDir(), iconName + ".png");
+                    Log.e("TAG","path = " + file.getAbsolutePath());
+                    if (file.exists()) {
+                        Log.e("TAG","path = exists" );
+                        image = BitmapFactory.decodeFile(getFilesDir() + "/" + iconName + ".png");
+                    } else {
+                        Log.e("TAG","path = download");
+                        URL imgUrl = new URL("https://openweathermap.org/img/w/" + iconName + ".png");
+                        HttpURLConnection connection = (HttpURLConnection) imgUrl.openConnection();
+                        connection.connect();
+                        int responseCode = connection.getResponseCode();
+                        if (responseCode == 200) {
+                            image = BitmapFactory.decodeStream(connection.getInputStream());
+                            image.compress(Bitmap.CompressFormat.PNG,100,openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
+                        }
+                    }
+                    Bitmap finalImage = image;
+                    runOnUiThread(() -> {
+                        TextView tv = findViewById(R.id.temp);
+                        tv.setText("The current temperature is " + current);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.minTemp);
+                        tv.setText("The min temperature is " + min);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.maxTemp);
+                        tv.setText("The max temperature is " + max);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.humidity);
+                        tv.setText("The humidity temperature is " + humidity + "%");
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.description);
+                        tv.setText(description);
+                        tv.setVisibility(View.VISIBLE);
+
+                        ImageView iv = findViewById(R.id.icon);
+                        iv.setImageBitmap(finalImage);
+                        iv.setVisibility(View.VISIBLE);
+                    });
+                } catch (IOException | JSONException ioe) {
+
+                }
+            });
         });
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
-    }
-
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w( "MainActivity", "In onStart() - The application is now visible on screen" );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w( "MainActivity", "In onResume() - The application is now responding to user input" );
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w( "MainActivity", "In onPause()- The application no longer responds to user input" );
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w( "MainActivity", "In onStop() - The application is no longer visible" );
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w( "MainActivity", "onDestroy() - Any memory used by the application is freed" );
     }
 }
