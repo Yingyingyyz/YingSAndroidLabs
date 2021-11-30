d4b4990e78dac6466f85dd64a49a04b185132945
ZoeyYingying <Zoey13652755443@gmail.com>
2021-11-29 19:17:28 -0700

    Finished Week9_XML

--- app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhao0211/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.BitmapCompat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
@@ -16,6 +17,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -24,6 +26,9 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -53,81 +58,136 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.forecastButton);
 
         btn.setOnClickListener((View v) -> {
+            String cityName = cityTextField.getText().toString();
+
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Getting forecast").setMessage("We're calling people in " + cityName +  " to look outside their windows and tell us what's the weather like over there.")
+                    .setView(new ProgressBar(MainActivity.this))
+                    .show();
+
             ExecutorService executorService = Executors.newSingleThreadExecutor();
             executorService.execute(() -> {
                 try {
                     // Los Angeles
-                    String cityName = cityTextField.getText().toString();
+
                     stringUrl = "https://api.openweathermap.org/data/2.5/weather?q="
                             + URLEncoder.encode(cityName, "UTF-8")
-                            + "&appid=7e943c97096a9784391a981c4d878b22&unit=metric";
+                            + "&appid=7e943c97096a9784391a981c4d878b22&unit=metric&mode=xml";
 
                     URL url = new URL(stringUrl);
                     HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                     InputStream in = httpURLConnection.getInputStream();
 
-                    String text = (new BufferedReader(
-                            new InputStreamReader(in, StandardCharsets.UTF_8)))
-                            .lines()
-                            .collect(Collectors.joining("\n"));
+                    /*
 
-                    JSONObject theDocument = new JSONObject(text);
+                     <current>
+                        <city id="5368361" name="Los Angeles">
+                        <coord lon="-118.2437" lat="34.0522"/>
+                        <country>US</country>
+                        <timezone>-28800</timezone>
+                        <sun rise="2021-11-26T14:36:26" set="2021-11-27T00:44:47"/>
+                        </city>
+                        <temperature value="289.49" min="285.53" max="292.5" unit="kelvin"/>
+                        <feels_like value="288.22" unit="kelvin"/>
+                        <humidity value="40" unit="%"/>
+                        <pressure value="1017" unit="hPa"/>
+                        <wind>
+                        <speed value="0" unit="m/s" name="Setting"/>
+                        <gusts/>
+                        <direction/>
+                        </wind>
+                        <clouds value="1" name="clear sky"/>
+                        <visibility value="10000"/>
+                        <precipitation mode="no"/>
+                        <weather number="800" value="clear sky" icon="01n"/>
+                        <lastupdate value="2021-11-27T03:10:26"/>
+                    </current>
+                     */
 
-                    JSONArray weatherArray = theDocument.getJSONArray("weather");
-                    JSONObject position0 = weatherArray.getJSONObject(0);
-                    String iconName = position0.getString("icon");
-                    String description = position0.getString("description");
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(false);
+                    XmlPullParser xpp = factory.newPullParser();
+                    xpp.setInput(in, "UTF-8");
+                    String current = "";
+                    String min = "";
+                    String max = "";
+                    String humidity = "";
+                    String iconName = "";
+                    String description = "";
+                    while (xpp.next() != XmlPullParser.END_DOCUMENT) {
+                        switch (xpp.getEventType()) {
+                            case XmlPullParser.START_TAG:
+                                if (xpp.getName().equals("temperature")) {
+                                    current = xpp.getAttributeValue(null, "value");  //this gets the current temperature
+                                    min = xpp.getAttributeValue(null, "min"); //this gets the min temperature
+                                    max = xpp.getAttributeValue(null, "max"); //this gets the max temperature
+                                } else if (xpp.getName().equals("weather")) {
+                                    description = xpp.getAttributeValue(null, "value");  //this gets the weather description
+                                    iconName = xpp.getAttributeValue(null, "icon"); //this gets the icon name
+                                } else if (xpp.getName().equals("humidity")) {
+                                    humidity = xpp.getAttributeValue(null, "value");  //this gets the humidity value
+                                }
+                                break;
 
-                    JSONObject mainObject = theDocument.getJSONObject("main");
-                    double current = mainObject.getDouble("temp");
-                    double min = mainObject.getDouble("temp_min");
-                    double max = mainObject.getDouble("temp_max");
-                    int humidity = mainObject.getInt("humidity");
+                            case XmlPullParser.END_TAG:
+                                break;
+
+                            case XmlPullParser.TEXT:
+                                break;
+                        }
+                    }
 
                     Bitmap image = null;
                     File file = new File(getFilesDir(), iconName + ".png");
-                    Log.e("TAG","path = " + file.getAbsolutePath());
+                    Log.e("TAG", "path = " + file.getAbsolutePath());
                     if (file.exists()) {
-                        Log.e("TAG","path = exists" );
+                        Log.e("TAG", "path = exists");
                         image = BitmapFactory.decodeFile(getFilesDir() + "/" + iconName + ".png");
                     } else {
-                        Log.e("TAG","path = download");
+                        Log.e("TAG", "path = download");
                         URL imgUrl = new URL("https://openweathermap.org/img/w/" + iconName + ".png");
                         HttpURLConnection connection = (HttpURLConnection) imgUrl.openConnection();
                         connection.connect();
                         int responseCode = connection.getResponseCode();
                         if (responseCode == 200) {
                             image = BitmapFactory.decodeStream(connection.getInputStream());
-                            image.compress(Bitmap.CompressFormat.PNG,100,openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
+                            image.compress(Bitmap.CompressFormat.PNG, 100, openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
                         }
                     }
                     Bitmap finalImage = image;
+                    String finalCurrent = current;
+                    String finalMin = min;
+                    String finalMax = max;
+                    String finalHumidity = humidity;
+                    String finalDescription = description;
                     runOnUiThread(() -> {
                         TextView tv = findViewById(R.id.temp);
-                        tv.setText("The current temperature is " + current);
+                        tv.setText("The current temperature is " + finalCurrent);
                         tv.setVisibility(View.VISIBLE);
 
                         tv = findViewById(R.id.minTemp);
-                        tv.setText("The min temperature is " + min);
+                        tv.setText("The min temperature is " + finalMin);
                         tv.setVisibility(View.VISIBLE);
 
                         tv = findViewById(R.id.maxTemp);
-                        tv.setText("The max temperature is " + max);
+                        tv.setText("The max temperature is " + finalMax);
                         tv.setVisibility(View.VISIBLE);
 
                         tv = findViewById(R.id.humidity);
-                        tv.setText("The humidity temperature is " + humidity + "%");
+                        tv.setText("The humidity temperature is " + finalHumidity + "%");
                         tv.setVisibility(View.VISIBLE);
 
                         tv = findViewById(R.id.description);
-                        tv.setText(description);
+                        tv.setText(finalDescription);
                         tv.setVisibility(View.VISIBLE);
 
                         ImageView iv = findViewById(R.id.icon);
                         iv.setImageBitmap(finalImage);
                         iv.setVisibility(View.VISIBLE);
+
+                        dialog.hide();
                     });
-                } catch (IOException | JSONException ioe) {
+                } catch (IOException | XmlPullParserException ioe) {
 
                 }
             });
